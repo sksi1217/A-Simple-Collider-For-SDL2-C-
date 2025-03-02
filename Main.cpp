e37513260c5c52e874e8d2b26cad1d1c330b9733
sksi1217 <pwllllll76@gmail.com>
2025-03-02 12:27:19 +0400

    Grid Optimization

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <SDL.h>
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <cstdlib>
 
 struct GameObject {
@@ -9,6 +10,7 @@ struct GameObject {
     float velocityY = 0.0f; // Скорость по Y
 };
 
+// Функция проверки столкновения двух прямоугольников
 bool CheckCollision(const SDL_Rect& rectA, const SDL_Rect& rectB) {
     return (rectA.x < rectB.x + rectB.w &&
         rectA.x + rectA.w > rectB.x &&
@@ -16,6 +18,7 @@ bool CheckCollision(const SDL_Rect& rectA, const SDL_Rect& rectB) {
         rectA.y + rectA.h > rectB.y);
 }
 
+// Функция разрешения столкновения
 void ResolveCollision(GameObject& objA, GameObject& objB) {
     int overlapX = 0;
     int overlapY = 0;
@@ -27,7 +30,7 @@ void ResolveCollision(GameObject& objA, GameObject& objB) {
         if (objA.rect.x < objB.rect.x) {
             objA.rect.x -= overlapX; // Коллизия слева
         }
-        else if (objA.rect.x > objB.rect.x) {
+        else {
             objA.rect.x += overlapX; // Коллизия справа
         }
     }
@@ -35,13 +38,66 @@ void ResolveCollision(GameObject& objA, GameObject& objB) {
         if (objA.rect.y < objB.rect.y) {
             objA.rect.y -= overlapY; // Коллизия сверху
         }
-        else if (objA.rect.y > objB.rect.y) {
+        else {
             objA.rect.y += overlapY; // Коллизия снизу
         }
     }
 
     objB.velocityX = -objB.velocityX;
     objB.velocityY = -objB.velocityY;
+}
+
+// Генерация сетки для оптимизации коллизий
+std::unordered_map<int, std::vector<GameObject*>> GenerateGrid(std::vector<GameObject>& objects, int cellSize, int width, int height) {
+    std::unordered_map<int, std::vector<GameObject*>> grid;
+
+    for (size_t i = 0; i < objects.size(); ++i) {
+        auto& obj = objects[i]; // Берем ссылку на объект
+        int minX = obj.rect.x / cellSize;
+        int maxX = (obj.rect.x + obj.rect.w - 1) / cellSize;
+        int minY = obj.rect.y / cellSize;
+        int maxY = (obj.rect.y + obj.rect.h - 1) / cellSize;
+
+        for (int y = minY; y <= maxY; ++y) {
+            for (int x = minX; x <= maxX; ++x) {
+                int cellIndex = y * (width / cellSize) + x;
+                grid[cellIndex].push_back(&objects[i]); // Добавляем указатель на объект
+            }
+        }
+    }
+
+    return grid;
+}
+
+// Проверка коллизий с использованием сетки
+void CheckCollisionsWithGrid(std::vector<GameObject>& objects, int cellSize, int width, int height) {
+    auto grid = GenerateGrid(objects, cellSize, width, height);
+    int collisionChecks = 0; // Счетчик проверок
+
+    for (const auto& [cellIndex, cellObjects] : grid) {
+        for (size_t i = 0; i < cellObjects.size(); ++i) {
+            for (size_t j = i + 1; j < cellObjects.size(); ++j) {
+                collisionChecks++;
+                if (CheckCollision(cellObjects[i]->rect, cellObjects[j]->rect)) {
+                    ResolveCollision(*cellObjects[i], *cellObjects[j]);
+                }
+            }
+        }
+    }
+
+    std::cout << "Number of collision checks: " << collisionChecks << std::endl;
+}
+
+void DrawGrid(SDL_Renderer* renderer, int cellSize, int width, int height) {
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Зеленый цвет для сетки
+
+    for (int x = 0; x < width; x += cellSize) {
+        SDL_RenderDrawLine(renderer, x, 0, x, height);
+    }
+
+    for (int y = 0; y < height; y += cellSize) {
+        SDL_RenderDrawLine(renderer, 0, y, width, y);
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -70,21 +126,27 @@ int main(int argc, char* argv[]) {
     // Добавляем объекты
     objects.push_back(GameObject{ {100, 100, 16, 16}, 0.0f, 0.0f });
     objects.push_back(GameObject{ {16, 16, 16, 16}, 0.0f, 0.0f });
-    objects.push_back(GameObject{ {16, 32, 16, 16}, 0.0f, 0.0f });
-    objects.push_back(GameObject{ {16, 48, 16, 16}, 0.0f, 0.0f });
-    objects.push_back(GameObject{ {16, 64, 16, 16}, 0.0f, 0.0f });
+    objects.push_back(GameObject{ {32, 100, 16, 16}, 0.0f, 0.0f });
+    objects.push_back(GameObject{ {16, 200, 16, 16}, 0.0f, 0.0f });
+    objects.push_back(GameObject{ {55, 75, 16, 16}, 0.0f, 0.0f });
     objects.push_back(GameObject{ {16, 80, 16, 16}, 0.0f, 0.0f });
 
+    /*
     objects.push_back(GameObject{ {16, 16, 16, 16}, 0.0f, 0.0f });
     objects.push_back(GameObject{ {32, 16, 16, 16}, 0.0f, 0.0f });
     objects.push_back(GameObject{ {48, 16, 16, 16}, 0.0f, 0.0f });
     objects.push_back(GameObject{ {64, 16, 16, 16}, 0.0f, 0.0f });
     objects.push_back(GameObject{ {80, 16, 16, 16}, 0.0f, 0.0f });
+    */
 
     bool running = true;
     SDL_Event event;
 
     const Uint8* keyboardState = SDL_GetKeyboardState(NULL); // Получаем состояние клавиш
+
+    int screenWidth = 800;
+    int screenHeight = 600;
+    int cellSize = 32; // Размер ячейки сетки
 
     while (running) {
         while (SDL_PollEvent(&event)) {
@@ -114,14 +176,8 @@ int main(int argc, char* argv[]) {
             obj.rect.y += obj.velocityY;
         }
 
-        // Проверка столкновений
-        for (size_t i = 0; i < objects.size(); ++i) {
-            for (size_t j = i + 1; j < objects.size(); ++j) {
-                if (CheckCollision(objects[i].rect, objects[j].rect)) {
-                    ResolveCollision(objects[i], objects[j]);
-                }
-            }
-        }
+        // Проверка столкновений с использованием сетки
+        CheckCollisionsWithGrid(objects, cellSize, screenWidth, screenHeight);
 
         // Очистка экрана
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -132,6 +188,9 @@ int main(int argc, char* argv[]) {
             SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Красный цвет
             SDL_RenderFillRect(renderer, &obj.rect);
         }
+
+        // Рисование сетки
+        DrawGrid(renderer, cellSize, screenWidth, screenHeight);
 
         // Отображение результата
         SDL_RenderPresent(renderer);
